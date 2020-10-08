b327ac85e529c73167dc0fdf61e0c8efc0c3e5bc
losttenko <losttenko@gmail.com>
2020-10-08 16:53:26 -0400

    hex

---
+++ keyboards/crkbd/rev1/common/keymaps/njdoyle/njdoyle.c
@@ -0,0 +1,190 @@
+#include "njdoyle.h"
+
+void njdoyle_input_switch(void) {
+	register_code(KC_LCTL);
+	register_code(KC_LALT);
+	register_code(KC_LGUI);
+	register_code(KC_LSFT);
+	tap_code(KC_SPC);
+	unregister_code(KC_LSFT);
+	unregister_code(KC_LGUI);
+	unregister_code(KC_LALT);
+	unregister_code(KC_LCTL);
+}
+
+bool njdoyle_process_record_user(uint16_t keycode, keyrecord_t* record) {
+	return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+	if (record->event.pressed) {
+		switch (keycode) {
+			// DEFAULT LAYERS
+			case D_QWRTY: set_single_persistent_default_layer(_QWERTY     ); return false;
+			case D_DVRAK: set_single_persistent_default_layer(_DVORAK     ); return false;
+			case D_COLEM: set_single_persistent_default_layer(_COLEMAK    ); return false;
+			case D_C_DH : set_single_persistent_default_layer(_COLEMAK_DH ); return false;
+			case D_WISH: set_single_persistent_default_layer(_WISH); return false;
+			case D_WORKM: set_single_persistent_default_layer(_WORKMAN    ); return false;
+
+			// EMOTES
+			case E_SMILE: {
+				njdoyle_input_switch();
+
+				register_code(KC_LALT);
+				tap_code(KC_3);
+				tap_code(KC_0);
+				tap_code(KC_C);
+				tap_code(KC_4);
+				unregister_code(KC_LALT);
+
+				njdoyle_input_switch();
+				return false;
+			}
+			case E_SHRUG: {
+				njdoyle_input_switch();
+
+				register_code(KC_LALT);
+				tap_code(KC_0);
+				tap_code(KC_0);
+				tap_code(KC_A);
+				tap_code(KC_F);
+				unregister_code(KC_LALT);
+
+				SEND_STRING("\\_(");
+
+				register_code(KC_LALT);
+				tap_code(KC_3);
+				tap_code(KC_0);
+				tap_code(KC_C);
+				tap_code(KC_4);
+				unregister_code(KC_LALT);
+
+				SEND_STRING(")_/");
+
+				register_code(KC_LALT);
+				tap_code(KC_0);
+				tap_code(KC_0);
+				tap_code(KC_A);
+				tap_code(KC_F);
+				unregister_code(KC_LALT);
+
+				njdoyle_input_switch();
+				return false;
+			}
+			case E_TFLIP: {
+				njdoyle_input_switch();
+
+				tap_code16(KC_LPRN);
+
+				register_code(KC_LALT);
+				tap_code(KC_2);
+				tap_code(KC_5);
+				tap_code(KC_6);
+				tap_code(KC_F);
+
+				tap_code(KC_0);
+				tap_code(KC_0);
+				tap_code(KC_B);
+				tap_code(KC_0);
+
+				tap_code(KC_2);
+				tap_code(KC_5);
+				tap_code(KC_A);
+				tap_code(KC_1);
+
+				tap_code(KC_0);
+				tap_code(KC_0);
+				tap_code(KC_B);
+				tap_code(KC_0);
+				unregister_code(KC_LALT);
+
+				tap_code16(KC_RPRN);
+
+				register_code(KC_LALT);
+				tap_code(KC_2);
+				tap_code(KC_5);
+				tap_code(KC_6);
+				tap_code(KC_F);
+
+				tap_code(KC_F);
+				tap_code(KC_E);
+				tap_code(KC_3);
+				tap_code(KC_5);
+				unregister_code(KC_LALT);
+
+				tap_code(KC_SPC);
+
+				register_code(KC_LALT);
+				tap_code(KC_2);
+				tap_code(KC_5);
+				tap_code(KC_3);
+				tap_code(KC_B);
+
+				tap_code(KC_2);
+				tap_code(KC_5);
+				tap_code(KC_0);
+				tap_code(KC_1);
+
+				tap_code(KC_2);
+				tap_code(KC_5);
+				tap_code(KC_3);
+				tap_code(KC_B);
+				unregister_code(KC_LALT);
+
+				njdoyle_input_switch();
+				return false;
+			}
+			case E_DERP: {
+				njdoyle_input_switch();
+
+				register_code(KC_LALT);
+				tap_code(KC_2);
+				tap_code(KC_6);
+				tap_code(KC_0);
+				tap_code(KC_6);
+
+				tap_code(KC_2);
+				tap_code(KC_3);
+				tap_code(KC_1);
+				tap_code(KC_2);
+				unregister_code(KC_LALT);
+
+				SEND_STRING("(> _ <)");
+
+				njdoyle_input_switch();
+				return false;
+			}
+			case E_DAPRV: {
+				njdoyle_input_switch();
+
+				tap_code16(KC_LPRN);
+
+				register_code(KC_LALT);
+				tap_code(KC_0);
+				tap_code(KC_C);
+				tap_code(KC_A);
+				tap_code(KC_0);
+				unregister_code(KC_LALT);
+
+				tap_code16(KC_UNDS);
+
+				register_code(KC_LALT);
+				tap_code(KC_0);
+				tap_code(KC_C);
+				tap_code(KC_A);
+				tap_code(KC_0);
+				unregister_code(KC_LALT);
+
+				tap_code16(KC_RPRN);
+
+				njdoyle_input_switch();
+				return false;
+			}
+		}
+
+	}
+
+	return njdoyle_process_record_user(keycode, record);
+}
+
---
+++ keyboards/crkbd/rev1/common/keymaps/njdoyle/njdoyle.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+// LAYERS
+#define _QWERTY      0
+#define _DVORAK      1
+#define _COLEMAK     2
+#define _COLEMAK_DH  3
+#define _WISH        4
+#define _WORKMAN     5
+#define _LOWER       6
+#define _RAISE       7
+#define _NAVIGATION  8
+#define _FUNCTION    9
+
+// KEY ALIASES
+#define L_LOWER MO(_LOWER)
+#define L_RAISE MO(_RAISE)
+#define L_FUNC  MO(_FUNCTION)
+#define DE_LEFT LCTL(KC_LEFT)
+#define DE_RGHT LCTL(KC_RIGHT)
+#define DE_MENU LCTL(KC_F2)
+#define TB_LEFT SGUI(KC_LBRC)
+#define TB_RGHT SGUI(KC_RBRC)
+#define TB_NEW  LGUI(KC_T)
+#define BR_BACK RGUI(KC_LBRC)
+#define BR_FRWD RGUI(KC_RBRC)
+#define BR_TOOL LCAG(KC_SLSH)
+#define KC_____ KC_TRNS
+#define KC_XXXX KC_NO
+
+enum njdoyle_keycodes {
+	// DEFAULT LAYERS
+	D_QWRTY = SAFE_RANGE, // QWERTY
+	D_DVRAK,              // Dvorak
+	D_COLEM,              // Colemak
+	D_C_DH ,              // Colemak Mod-DH
+	D_WISH,              // Colemak Mod-DHm
+	D_WORKM,              // Workman
+
+	// EMOTES
+	E_SMILE, // Smile
+	E_SHRUG, // Shrug
+	E_TFLIP, // Table Flip
+	E_DERP , // Derp
+	E_DAPRV, // Disapprove
+
+	// UTILITY
+	NJDOYLE_SAFE_RANGE
+};
+
+#include "njdoyle_layout_base.h"
+
+#define NJDOYLE_LAYOUT_BASE \
+	[_QWERTY] = NJDOYLE_LAYOUT_BASE_KC( \
+		Q, W, E, R, T, Y, U, I   , O  , P   , \
+		A, S, D, F, G, H, J, K   , L  , SCLN, \
+		Z, X, C, V, B, N, M, COMM, DOT, SLSH  \
+	), \
+	[_DVORAK] = NJDOYLE_LAYOUT_BASE_KC( \
+		SLSH, COMM, DOT, P, Y, F, G, C, R, L, \
+		A   , O   , E  , U, I, D, H, T, N, S, \
+		SCLN, Q   , J  , K, X, B, M, W, V, Z  \
+	), \
+	[_COLEMAK] = NJDOYLE_LAYOUT_BASE_KC( \
+		Q, W, F, P, G, J, L, U   , Y  , SCLN, \
+		A, R, S, T, D, H, N, E   , I  , O   , \
+		Z, X, C, V, B, K, M, COMM, DOT, SLSH  \
+	), \
+	[_COLEMAK_DH] = NJDOYLE_LAYOUT_BASE_KC( \
+		Q, W, F, P, B, J, L, U   , Y  , SCLN, \
+		A, R, S, T, G, K, N, E   , I  , O   , \
+		Z, X, C, D, V, M, H, COMM, DOT, SLSH  \
+	), \
+	[_WORKMAN] = NJDOYLE_LAYOUT_BASE_KC( \
+		Q, D, R, W, B, J, F, U   , P  , SCLN, \
+		A, S, H, T, G, Y, N, E   , O  , I   , \
+		Z, X, M, C, V, K, L, COMM, DOT, SLSH  \
+	), \
+	[_WISH] = NJDOYLE_LAYOUT_BASE_KC( \
+		J, P, U, O, K, B, D, R   , F  ,Q, \
+		W, I, E, A, DOT, G, T, N   , S  , H,   \
+		SLSH, Y, SCLN, COMM, Z, V, M, L, C, X  \
+	)
+
+
+bool njdoyle_process_record_user(uint16_t, keyrecord_t*) __attribute__((weak));
